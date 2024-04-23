79cf2c3f4b43a97eb38eb3e220023740437d62c6
ammmar <ammaraslam7164@gmail.com>
2024-04-23 17:15:29 +0600

    banner-fix-all-overlay

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -8,6 +8,39 @@ const Banner = () => {
   return (
     <div className="carousel w-full">
       <div id="slide1" className="carousel-item relative w-full">
+        <img src={banner_4} className="w-full rounded-xl h-[600px] " />
+
+        <div className="absolute left-0 top-0  h-full  w-full bg-gradient-to-r from-[#151515] to-white-0 rounded-xl">
+          <div className="space-y-6 ml-9">
+            <p className="text-2xl font-semibold mt-32 text-white">
+              ASSALAMUALAIKUM, WELCOME TO
+            </p>
+            <h2 className="text-5xl  font-bold text-white ">
+              Mahadul Qira'at Al Hind
+            </h2>
+            <p className="text-2xl font-semibold  text-orange-500">
+              Best Online Quran and Qira'at Learning <br /> Platform (English,
+              Urdu, Hindi, Bangla, and Arabic Medium)
+            </p>
+            <Link to="/form">
+              {" "}
+              <button className="btn btn-xs hover:bg-[#1a103d] hover:text-white hover:text-xl mt-9 btn-sm md:btn-md lg:btn-lg outline outline-orange-500 hover:outline-green-500 text-orange-500 bg-transparent ">
+                Admission Open
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="absolute flex right-10 bottom-14 md:top-3/4">
+          <a href="#slide4" className="btn btn-circle bg-white mr-5">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle bg-white">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
         <img src={banner_1} className="w-full rounded-xl h-[600px] " />
 
         <div className="absolute left-0 top-0  h-full  w-full bg-gradient-to-r from-[#151515] to-white-0 rounded-xl">
@@ -22,23 +55,15 @@ const Banner = () => {
               Best Online Quran and Qira'at Learning <br /> Platform (English,
               Urdu, Hindi, Bangla, and Arabic Medium)
             </p>
-           <Link to="/form"> <button className="btn btn-xs mt-9 sm:btn-sm md:btn-md lg:btn-lg outline outline-orange-500 hover:outline-green-500 text-orange-500 bg-transparent hover:bg-[#1a103d] hover:text-white hover:text-xl">
-              Admission Open
-            </button></Link>
+            <Link to="/form">
+              {" "}
+              <button className="btn btn-xs hover:bg-[#1a103d] hover:text-white hover:text-xl mt-9 btn-sm md:btn-md lg:btn-lg outline outline-orange-500 hover:outline-green-500 text-orange-500 bg-transparent ">
+                Admission Open
+              </button>
+            </Link>
           </div>
         </div>
-        <div className="absolute flex right-10 top-3/4">
-          <a href="#slide4" className="btn btn-circle bg-white mr-5">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle bg-white">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src={banner_2} className="w-full rounded-xl h-[600px]" />
-        <div className="absolute flex justify-end right-10   top-1/3">
+        <div className="absolute flex right-10 bottom-14 md:top-3/4">
           <a href="#slide1" className="btn btn-circle bg-white mr-5">
             ❮
           </a>
@@ -48,8 +73,29 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
-        <img src={banner_3} className="w-full rounded-xl h-[600px]" />
-        <div className="absolute flex justify-end right-10   top-1/3">
+        <img src={banner_3} className="w-full rounded-xl h-[600px] " />
+
+        <div className="absolute left-0 top-0  h-full  w-full bg-gradient-to-r from-[#151515] to-white-0 rounded-xl">
+          <div className="space-y-6 ml-9">
+            <p className="text-2xl font-semibold mt-32 text-white">
+              ASSALAMUALAIKUM, WELCOME TO
+            </p>
+            <h2 className="text-5xl  font-bold text-white ">
+              Mahadul Qira'at Al Hind
+            </h2>
+            <p className="text-2xl font-semibold  text-orange-500">
+              Best Online Quran and Qira'at Learning <br /> Platform (English,
+              Urdu, Hindi, Bangla, and Arabic Medium)
+            </p>
+            <Link to="/form">
+              {" "}
+              <button className="btn btn-xs hover:bg-[#1a103d] hover:text-white hover:text-xl mt-9 btn-sm md:btn-md lg:btn-lg outline outline-orange-500 hover:outline-green-500 text-orange-500 bg-transparent ">
+                Admission Open
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="absolute flex right-10 bottom-14 md:top-3/4">
           <a href="#slide2" className="btn btn-circle bg-white mr-5">
             ❮
           </a>
@@ -59,8 +105,29 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
-        <img src={banner_4} className="w-full rounded-xl h-[600px]" />
-        <div className="absolute flex justify-end right-10   top-1/3">
+        <img src={banner_2} className="w-full rounded-xl h-[600px] " />
+
+        <div className="absolute left-0 top-0  h-full  w-full bg-gradient-to-r from-[#151515] to-white-0 rounded-xl">
+          <div className="space-y-6 ml-9">
+            <p className="text-2xl font-semibold mt-32 text-white">
+              ASSALAMUALAIKUM, WELCOME TO
+            </p>
+            <h2 className="text-5xl  font-bold text-white ">
+              Mahadul Qira'at Al Hind
+            </h2>
+            <p className="text-2xl font-semibold  text-orange-500">
+              Best Online Quran and Qira'at Learning <br /> Platform (English,
+              Urdu, Hindi, Bangla, and Arabic Medium)
+            </p>
+            <Link to="/form">
+              {" "}
+              <button className="btn btn-xs hover:bg-[#1a103d] hover:text-white hover:text-xl mt-9 btn-sm md:btn-md lg:btn-lg outline outline-orange-500 hover:outline-green-500 text-orange-500 bg-transparent ">
+                Admission Open
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="absolute flex right-10 bottom-14 md:top-3/4">
           <a href="#slide3" className="btn btn-circle bg-white mr-5">
             ❮
           </a>
