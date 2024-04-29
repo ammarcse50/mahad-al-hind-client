6deb556affa2e096ac61ec4c9c94c31cac4b0eeb
ammmar <ammaraslam7164@gmail.com>
2024-04-29 11:05:43 +0600

    card button hvoer fix

--- src/pages/Home/Courses/CourseCard.jsx
+++ src/pages/Home/Courses/CourseCard.jsx
@@ -11,7 +11,7 @@ const CourseCard = ({course}) => {
           <h2 className="card-title">{course.title}</h2>
           <p>{course.desc}</p>
           <div className="card-actions justify-end">
-           <Link to={course.contact}><button className="btn rounded-xl text-black bg-orange-600 text-white hover:bg-green-500">Contact</button></Link>
+           <Link to={course.contact}><button className="btn rounded-xl text-white bg-orange-600 hover:shadow-xl hover:shadow-[#0ecb34] hover:bg-[#0ecb34]">Contact</button></Link>
           </div>
         </div>
       </div>
