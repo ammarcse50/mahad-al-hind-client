68ee4df050bc92c9d621fa327404ddcba5c84bb7
ammmar <ammaraslam7164@gmail.com>
2024-08-02 15:47:33 +0600

    category ui fixed

--- src/pages/Home/Category/CategoryCard.jsx
+++ src/pages/Home/Category/CategoryCard.jsx
@@ -2,14 +2,14 @@ const CategoryCard = ({ item }) => {
   const { cover,contact,desc,title} = item;
 
   return (
-    <div className="card card-compact bg-base-100 w-96 shadow-xl">
+    <div className="card card-compact bg-base-100 w-96  p-10 md:block  shadow-xl">
       <figure>
         <img src={cover} alt="upload" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <p>{desc}</p>
-        <div className="card-actions justify-end pb-5">
+        <div className="card-actions justify-end ">
         <a href={contact}><button className="btn btn-primary">Enroll Now</button></a>
           
         </div>
